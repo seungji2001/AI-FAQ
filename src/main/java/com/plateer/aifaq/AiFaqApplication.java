acf1233f6ece93ca74db2f7312d5b6176d561ec9
seungjibaek <seungjibaek@Seungjiui-MacBookAir.local>
2025-12-13 14:52:40 +0900

    first commit

---
+++ src/main/java/com/plateer/aifaq/AiFaqApplication.java
@@ -0,0 +1,13 @@
+package com.plateer.aifaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFaqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFaqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plateer/aifaq/AiFaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plateer.aifaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFaqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
